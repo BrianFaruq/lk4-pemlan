21d175b70b36bf1a33b74166a695ae8baa47dcad
BrianFaruq <brian.faruqw16@gmail.com>
2026-03-29 20:00:34 +0700

    Initial work

--- TransaksiDigital.java
+++ TransaksiDigital.java
@@ -1,3 +1,6 @@
 public interface  TransaksiDigital extends Transaksi {
+   // Transaksi digital butuh keamanan tambahan seperti OTP/Token
+   void autentikasiDuaFaktor();
+   String getPlatformDigital(); // Misal "Mobile Banking" atau "QRIS"
 
-   }
+}
--- TransferGlobal.java
+++ TransferGlobal.java
@@ -1,3 +1,4 @@
 public interface TransferGlobal extends LayananInternasional, TransaksiDigital {
+    void kirimKeLuarNegeri();
 
 }
