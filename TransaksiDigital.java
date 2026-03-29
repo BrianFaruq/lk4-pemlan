1d4d0525ec804743b9c8a28dfc64cfdd26b231ea
rafianantaadi-jpg <rafianantaadi@gmail.com>
2026-03-29 19:20:11 +0700

    Menambahkan method-method dasar

--- TransaksiDigital.java
+++ TransaksiDigital.java
@@ -1,3 +1,5 @@
-public interface  TransaksiDigital extends Transaksi {
-
-   }
+public interface TransaksiDigital extends Transaksi {
+    // Transaksi digital butuh keamanan tambahan seperti OTP/Token
+    void autentikasiDuaFaktor();
+    String getPlatformDigital(); // Misal: "Mobile Banking" atau "QRIS"
+}
